e2bcb4724714d517c91f851a4172c7814d286f75
Jisse Reitsma <jisse@yireo.com>
2020-11-22 13:34:38 +0100

    Pending changes

--- bin/docker_elasticsearch.sh
+++ bin/docker_elasticsearch.sh
@@ -19,7 +19,7 @@ docker run \
     -p 9200:9200 \
     -p 9300:9300 \
     -e "discovery.type=single-node" \
-    -e "ES_JAVA_OPTS=-Xms256m -Xmx256m" \
+    -e "ES_JAVA_OPTS=-Xms128m -Xmx128m" \
     --cpus=1 \
     --net=magento \
     --ip=172.20.0.105 \
